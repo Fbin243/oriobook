6c4def99c2be903cee47ab5ebafacd40b556e6b9
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-11-18 12:03:32 +0700

    Finish header

--- front-end/src/components/Nav.vue
+++ front-end/src/components/Nav.vue
@@ -11,12 +11,27 @@
         <a href="#" class="nav-link"><span>Products</span></a>
       </li>
       <li class="nav-item">
-        <a href="#" class="nav-link">
+        <a href="#" class="nav-link"><span>Authors</span></a>
+      </li>
+      <li class="nav-item sub-nav-container">
+        <a href="#" class="nav-link sub-nav-container">
           <span class="d-flex align-items-center">
             More
-            <i class="fa-regular fa-angle-down drop-down-icon"></i>
+            <i class="fa-regular fa-angle-down down-icon"></i>
+            <i class="fa-regular fa-angle-up up-icon"></i>
           </span>
         </a>
+        <ul class="sub-nav">
+          <li class="nav-item">
+            <a href="" class="nav-link">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">FAQ</a>
+          </li>
+        </ul>
       </li>
     </ul>
     <ul class="nav-list">

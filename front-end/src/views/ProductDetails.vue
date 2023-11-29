e3ed426f46b3792eb1da72ae4d0f6d205a3c8b21
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-11-29 18:06:50 +0700

    Finish product detail

--- front-end/src/views/ProductDetails.vue
+++ front-end/src/views/ProductDetails.vue
@@ -1,6 +1,79 @@
 <template>
   <div class="product-details container">
-    <div class="row">
+    <div class="breadcrumb" itemprop="breadcrumb">
+      <a href="/">Home</a>
+      <span class="delimiter"></span>
+      <a href="/shop">Shop</a>
+      <span class="delimiter"></span>
+      <a href="/shop">Romance</a>
+      <span class="delimiter"></span>
+      I Don’t Forget Anyone
+    </div>
+
+    <div class="product-content row">
+      <div class="product-img col-6">
+        <img src="@/assets/img/products/product-76.jpg" alt="" />
+      </div>
+      <div class="product-info col-6">
+        <div class="product-title">I Don’t Forget Anyone</div>
+        <div class="product-price">100.00$</div>
+        <p class="product-desc">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur.
+        </p>
+        <div class="d-flex">
+          <div class="product-quantity row gx-0 me-2">
+            <button class="col">
+              <i class="fa-light fa-minus"></i>
+            </button>
+            <input type="text" class="col text-center" value="5" />
+            <button class="col">
+              <i class="fa-light fa-plus"></i>
+            </button>
+          </div>
+          <button class="product-add-cart-btn">
+            <span class="text-uppercase">Add to cart</span>
+          </button>
+        </div>
+        <button class="product-quick-buy-btn">
+          <span class="text-uppercase">Buy now</span>
+        </button>
+
+        <ul class="product-shipping-delivers d-flex">
+          <li class="product-shipping">
+            <div class="content-info">
+              <div class="content">
+                <i class="fa-light fa-box"></i>
+                <p>Free worldwide shipping on all orders over $100</p>
+              </div>
+            </div>
+          </li>
+          <li class="product-delivers">
+            <div class="content-info">
+              <div class="content">
+                <i class="fa-light fa-clock-three"></i>
+                <p>
+                  Delivers in: 3-7 Working Days
+                  <a href="/refund-policy">Shipping &amp; Return </a>
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <div class="safe-checkout">
+          <div class="title-safe-checkout">Guaranteed Checkout</div>
+          <div class="img-safe-checkout">
+            <img
+              src="https://wpbingosite.com/wordpress/oriobook/wp-content/themes/oriobook/images/payment-product.png"
+              alt="Image Checkout"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5">
       <div class="col-12 title">Related Products</div>
 
       <div class="row">
@@ -18,7 +91,7 @@ import HomeProductCard from "@/components/product/HomeProductCard.vue";
 
 export default {
   name: "ProductDetails",
-  components:{
+  components: {
     HomeProductCard,
   },
   setup() {

53b7991a6a3c40bc00e0a5f65445a6ae34bcab4f
Albert de Nafta <treblaatfan1406@gmail.com>
2023-11-29 21:17:06 +0700

    Fixes

--- front-end/src/components/Category.vue
+++ front-end/src/components/Category.vue
@@ -2,66 +2,106 @@
   <div class="grid-container">
     <!-- First Column -->
     <div class="big-card">
-      <img
-        src="../assets/img/56732449.jpg"
-        style="width: 60%; padding-top: 100px; padding-bottom: 100px"
-      />
-      <h2>Romanace</h2>
+      <div class="card-content">
+        <a href="#">
+          <img
+            src="../assets/img/romance.jpg"
+            style="width: 80%; padding-top: 100px"
+          />
+          <h2>Romanace</h2></a
+        >
+      </div>
     </div>
 
     <!-- Second Column -->
     <div class="small-card">
-      <img
-        src="../assets/img/fic1.jpg"
-        style="width: 25%; padding-bottom: 50px; padding-left: 20px"
-      />
-      <img
-        src="../assets/img/fic2.jpg"
-        style="width: 25%; padding-bottom: 50px; padding-left: 20px"
-      />
-      <img
-        src="../assets/img/fic3.jpg"
-        style="
-          width: 25%;
-          padding-bottom: 50px;
-          padding-left: 20px;
-          height: 300px;
-        "
-      />
-      <h2 style="text-align: left; padding-left: 50px; padding-bottom: 50px">
-        Fiction
-      </h2>
+      <div class="card-content">
+        <a href="#">
+          <img
+            src="../assets/img/fiction.jpg"
+            style="
+              width: 90%;
+              height: 230px;
+              padding-bottom: 50px;
+              padding-left: 20px;
+            "
+          />
+          <h2 style="text-align: left; padding-left: 50px; padding-top: 0px">
+            Fiction
+          </h2></a
+        >
+      </div>
     </div>
 
     <div class="small-card">
-      <div style="text-align: right">
-        <img
-          src="../assets/img/download.jpg"
-          style="width: 45%; padding-top: 50px; padding-right: 50px"
-        />
+      <div class="card-content">
+        <a href="#">
+          <div style="text-align: right">
+            <img
+              src="../assets/img/family.jpg"
+              style="width: 55%; padding-top: 50px; padding-right: 50px"
+            />
+          </div>
+          <h2
+            style="
+              text-align: left;
+              padding-left: 50px;
+              padding-bottom: 50px;
+              margin-top: -50px;
+            "
+          >
+            Family
+          </h2></a
+        >
       </div>
-      <h2 style="text-align: left; padding-left: 50px; padding-bottom: 50px">
-        Family
-      </h2>
     </div>
 
     <!-- Third Column -->
 
     <div class="small-card">
-      <div style="text-align: right">
-        <img
-          src="../assets/img/comedy.jpg"
-          style="width: 45%; padding-top: 50px; padding-right: 50px"
-        />
+      <div class="card-content">
+        <a href="#">
+          <div style="text-align: right">
+            <img
+              src="../assets/img/comedy.jpg"
+              style="width: 55%; padding-top: 50px; padding-right: 50px"
+            />
+          </div>
+          <h2
+            style="
+              text-align: left;
+              padding-left: 50px;
+              padding-bottom: 50px;
+              margin-top: -50px;
+            "
+          >
+            Comedy
+          </h2></a
+        >
       </div>
-      <h2 style="text-align: left; padding-left: 50px; padding-bottom: 50px">
-        Comedy
-      </h2>
     </div>
 
     <div class="small-card">
-      <h2>Small Card 6</h2>
-      <p>This is the content of small card 6.</p>
+      <div class="card-content">
+        <a href="#">
+          <div style="text-align: right">
+            <img
+              src="../assets/img/history.jpg"
+              style="width: 55%; padding-top: 50px; padding-right: 50px"
+            />
+          </div>
+          <h2
+            style="
+              text-align: left;
+              padding-left: 50px;
+              padding-bottom: 50px;
+              margin-top: -50px;
+            "
+          >
+            History
+          </h2></a
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -73,5 +113,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/styles/Category.scss";
+@import "../styles/Category.scss";
 </style>
---
+++ front-end/src/components/Contact.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="map" style="width: 50%;margin: 30px;"></div>
+    <div class="container">
+        <h4>Need more information?</h4>
+        <br>
+        <div><i class="fa-solid fa-location-dot"></i> 49 Boughtman Street Huntidale, Victoria(VIC),<br> 3166 (03) 9238 8831
+        </div>
+        <br>
+        <div>For all customer enquiries please contact us at: hello@hazelandfolk.com<br>
+            Please allow 24 to 48 hours for our team to respond to you.</div><br>
+        <h4>Open hours:</h4>
+        Monday to Friday 8am to 5pm,<br> Australian Eastern Standard Time.<br><br>
+        <h4>Contact us:</h4>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-dark btn-hover-orange">Submit</button>
+
+        </form>
+    </div>
+</template>
+  
+<script>
+import 'jquery';
+
+export default {
+    name: 'Contact',
+    methods: {
+        loadMapScenario() {
+            const mapOptions = {
+                credentials: 'Agi_xIfVmXm2fvEGclgCdvhCRj_g8BbpNU092IuU6RCrO8eJ5IMaXHgDH5K13E3g',
+                center: new Microsoft.Maps.Location(10.7638, 106.6823),
+                zoom: 16
+            };
+
+            const map = new Microsoft.Maps.Map(document.getElementById('map'), mapOptions);
+
+            // You can add additional map functionality or markers here
+        }
+    },
+    mounted() {
+    window.loadBingMaps = this.loadMapScenario; // Define the callback function before creating the script
+
+    // Load the Bing Maps script dynamically
+    const script = document.createElement('script');
+    script.src = 'https://www.bing.com/api/maps/mapcontrol?callback=loadBingMaps';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+
+};
+</script>
+  
+<style lang="scss" scoped>
+@import "../styles/Contact.scss";
+</style>
+  

57b0eb5528b9bd9b4ef9d774f934707a4c929fb9
Albert de Nafta <treblaatfan1406@gmail.com>
2023-11-30 22:14:41 +0700

    Only checkout

---
+++ front-end/src/components/tabProduct.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container mt-5">
+        <ul class="nav nav-underline d-flex justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" id="tab1" data-toggle="tab" href="#content1"
+                    style="font-family: Jost,sans-serif;font-size: 29px;font-weight: 100;">Description</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="tab2" data-toggle="tab" href="#content2"
+                    style="font-family: Jost,sans-serif;font-size: 29px;font-weight: 100;">Review</a>
+            </li>
+        </ul>
+
+        <div class="tab-content mt-2">
+            <div id="content1" class="tab-pane fade show active">
+                <!-- Your content for the 'Active' link goes here -->
+                <br><br>
+                <p style="font-size: 15px;color: #242424CC;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.</p>
+            </div>
+            <div id="content2" class="tab-pane fade">
+                <!-- Your content for the 'Link' goes here -->
+                <p style="font-family: Lora,serif;font-size: 25px;color: #242424;" >Reviews</p><br>
+                <p style="font-family: Jost,sans-serif;font-size: 15px;color: #242424CC;" >There is no review yet</p>
+                <button type="button" class="btn" style="font-size: 15px;">Write a review</button>    
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "tabProduct",
+};
+</script>
+
+<style lang="scss" scoped>@import "../styles/tabProduct.scss";</style>
+
--- front-end/src/views/aboutus.vue
+++ front-end/src/views/aboutus.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="shop-product" style = "margin-bottom: 500px;">
+    <div class="shop-page" style = "margin-bottom: 3800px;">
       <div class="img-cover">
         <h1 class="title-heading">About us</h1>
         <br><br><br><br>
         <featureRow />
+        <vidOffer />
+        <ourHisAndTeam/>
         <Testimonial />
+        <br><br>
+        <emailSupscription />
       </div>
     </div>
     <footers />
@@ -14,8 +18,12 @@
   import Testimonial from "@/components/Testimonial.vue";
   import Contact from '@/components/Contact.vue';
   import featureRow from "@/components/featureRow2.vue";
-
+  import ourHisAndTeam from "@/components/ourHisAndTeam.vue";
   import footers from "@/components/footer.vue";
+  import vidOffer from "@/components/vidOffer.vue";
+  import emailSupscription from "@/components/emailSupscription.vue";
+
+
 
   
   export default {
@@ -24,6 +32,9 @@
         footers,
         featureRow,
         Testimonial,
+        ourHisAndTeam,
+        vidOffer,
+        emailSupscription,
     }
   }
   </script>

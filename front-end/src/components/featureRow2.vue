57a4bfe69e4f0f8241982f0e96976f05b9beb2b0
Albert de Nafta <treblaatfan1406@gmail.com>
2023-11-30 15:57:29 +0700

    Doing aboutus

---
+++ front-end/src/components/featureRow2.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+         <div class="box">
+             <img src="../assets/img/img-30.png" style="width: 100px;"><br><br>Free Standard Delivery<br><br>
+             <div class="reset">Yes, we delivery our books to you guys very fast, dont need to wait</div>
+         </div>
+         <div class="box">
+            <img src="../assets/img/img-31.png" style="width: 100px;"><br><br>Free Easy Returns<br><br>
+             <div class="reset">No, we just say it for fun, we dont have any policy that help you return what you bought :></div>
+         </div>
+         <div class="box">
+            <img src="../assets/img/img-32.png" style="width: 100px;"><br><br>6 Million Books Available<br><br>
+             <div class="reset">Nah, we are not a library, only a few books are available here</div>
+         </div>
+         <div class="box">
+            <img src="../assets/img/img-33.png" style="width: 100px;"><br><br>Special Discounts<br><br>
+             <div class="reset">Jokes on you, there will be no discount and you will not have any book delivered</div>
+         </div>
+     </div>
+</template>
+
+<script>
+export default {
+ name: "featureRow",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/featureRow.scss";
+</style>
---
+++ front-end/src/views/aboutus.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="shop-product" style = "margin-bottom: 500px;">
+      <div class="img-cover">
+        <h1 class="title-heading">About us</h1>
+        <br><br><br><br>
+        <featureRow />
+        <Testimonial />
+      </div>
+    </div>
+    <footers />
+  </template>
+  
+  <script>
+  import Testimonial from "@/components/Testimonial.vue";
+  import Contact from '@/components/Contact.vue';
+  import featureRow from "@/components/featureRow2.vue";
+
+  import footers from "@/components/footer.vue";
+
+  
+  export default {
+    components: {
+        Contact,
+        footers,
+        featureRow,
+        Testimonial,
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  @import "@/styles/shop/shop.scss";
+  </style>

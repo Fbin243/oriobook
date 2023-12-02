467b615f6ce67aba30594406cc4f51bb44d16ed0
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-12-02 23:27:48 +0700

    Fix 2

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -14,18 +14,19 @@ import Deal_TopRated from "@/components/home/Home_Deal_TopRated.vue";
 import HomeProduct from "@/components/home/HomeProductSection.vue";
 import HomeBanner from "@/components/home/HomeBanner.vue";
 import featureRow from "@/components/featureRow.vue";
-import Categogy from "@/components/Categogy.vue";
+import Category from "@/components/Category.vue";
 import Testimonial from "@/components/Testimonial.vue";
 
 export default {
   name: "Home",
   components: {
     Hero,
+    featureRow,
+    Category,
     Deal_TopRated,
     HomeProduct,
     HomeBanner,
     featureRow,
-    Categogy,
     Testimonial,
   },
 };

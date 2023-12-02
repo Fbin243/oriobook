1ee5df2612830ce0d7a77918e4fe76e38d3c93d5
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-12-02 23:27:48 +0700

    Fix 2

---
+++ front-end/src/components/Category.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="grid-container">
+    <!-- First Column -->
+    <div class="big-card">
+      <img
+        src="../assets/img/56732449.jpg"
+        style="width: 60%; padding-top: 100px; padding-bottom: 100px"
+      />
+      <h2>Romanace</h2>
+    </div>
+
+    <!-- Second Column -->
+    <div class="small-card">
+      <img
+        src="../assets/img/fic1.jpg"
+        style="width: 25%; padding-bottom: 50px; padding-left: 20px"
+      />
+      <img
+        src="../assets/img/fic2.jpg"
+        style="width: 25%; padding-bottom: 50px; padding-left: 20px"
+      />
+      <img
+        src="../assets/img/fic3.jpg"
+        style="
+          width: 25%;
+          padding-bottom: 50px;
+          padding-left: 20px;
+          height: 300px;
+        "
+      />
+      <h2 style="text-align: left; padding-left: 50px; padding-bottom: 50px">
+        Fiction
+      </h2>
+    </div>
+
+    <div class="small-card">
+      <div style="text-align: right">
+        <img
+          src="../assets/img/download.jpg"
+          style="width: 45%; padding-top: 50px; padding-right: 50px"
+        />
+      </div>
+      <h2 style="text-align: left; padding-left: 50px; padding-bottom: 50px">
+        Family
+      </h2>
+    </div>
+
+    <!-- Third Column -->
+
+    <div class="small-card">
+      <div style="text-align: right">
+        <img
+          src="../assets/img/comedy.jpg"
+          style="width: 45%; padding-top: 50px; padding-right: 50px"
+        />
+      </div>
+      <h2 style="text-align: left; padding-left: 50px; padding-bottom: 50px">
+        Comedy
+      </h2>
+    </div>
+
+    <div class="small-card">
+      <h2>Small Card 6</h2>
+      <p>This is the content of small card 6.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/Category.scss";
+</style>
--- front-end/src/components/featureRow.vue
+++ front-end/src/components/featureRow.vue
@@ -1,31 +1,43 @@
 <template>
-       <div class="container">
-            <div class="box">
-                <i class="icon fas fa-truck-moving"></i><br><br>Free Standard Delivery<br><br>
-                <div class="reset">Yes, we delivery our books to you guys very fast, dont need to wait</div>
-            </div>
-            <div class="box">
-                <i class="icon fas fa-undo-alt"></i><br><br>Free Easy Returns<br><br>
-                <div class="reset">No, we just say it for fun, we dont have any policy that help you return what you bought :></div>
-            </div>
-            <div class="box">
-                <i class="icon fas fa-journal-whills"></i><br><br>6 Million Books Available<br><br>
-                <div class="reset">Nah, we are not a library, only a few books are available here</div>
-            </div>
-            <div class="box">
-                <i class="icon fas fa-hand-holding-usd"></i><br><br>Special Discounts<br><br>
-                <div class="reset">Jokes on you, there will be no discount and you will not have any book delivered</div>
-            </div>
-        </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "featureRow",
-  };
-  </script>
-  
-  <style lang="scss" scoped>
-  @import "../styles/featureRow.scss";
-  </style>
-  
+  <div class="container">
+    <div class="box">
+      <i class="icon fas fa-truck-moving"></i><br /><br />Free Standard
+      Delivery<br /><br />
+      <div class="reset">
+        Yes, we delivery our books to you guys very fast, dont need to wait
+      </div>
+    </div>
+    <div class="box">
+      <i class="icon fas fa-undo-alt"></i><br /><br />Free Easy Returns<br /><br />
+      <div class="reset">
+        No, we just say it for fun, we dont have any policy that help you return
+        what you bought :>
+      </div>
+    </div>
+    <div class="box">
+      <i class="icon fas fa-journal-whills"></i><br /><br />6 Million Books
+      Available<br /><br />
+      <div class="reset">
+        Nah, we are not a library, only a few books are available here
+      </div>
+    </div>
+    <div class="box">
+      <i class="icon fas fa-hand-holding-usd"></i><br /><br />Special
+      Discounts<br /><br />
+      <div class="reset">
+        Jokes on you, there will be no discount and you will not have any book
+        delivered
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "featureRow",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/featureRow.scss";
+</style>

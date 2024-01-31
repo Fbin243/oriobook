160aff165246ff02a6ead30299498bd88debda1d
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-01-31 14:27:42 +0700

    update 0.0.0.0

--- back-end/aux-sys/src/server.js
+++ back-end/aux-sys/src/server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 
 database.connect();
 const port = process.env.PORT || 4000;
-app.listen(port, "127.0.0.1", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Payment server is running on http://localhost:${port}`);
 });
